5643a1a210fc447741dfe7fc4e01c3c31587a48b
Christopher Carrasco <chris.carrasco@att.net>
2023-02-06 18:07:26 -0600

    Configured project to connect to a postgres db running on Docker but getting a 'Failed to configure a DataSource' error despite following video, video time stamp 1:32:40

--- src/main/java/com/guymanpersonboy/springboot2/Springboot2Application.java
+++ src/main/java/com/guymanpersonboy/springboot2/Springboot2Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 // Specify to Spring to scan for specific modules here.
-@ComponentScan({"api", "dao", "model", "service"})
+@ComponentScan({"api", "dao", "datasource", "model", "service"})
 public class Springboot2Application {
 
 	public static void main(String[] args) {
